232f2acd0786cc7fe19aef5be4b26083d53395ed
MzMahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2019-01-31 11:29:19 +0600

    play_with_randomPlayer added

--- 4. Mancala/main.cpp
+++ 4. Mancala/main.cpp
@@ -2,7 +2,7 @@
 
 //returns the best move for player
 #define INF INT_MAX
-#define MAX_DEPTH 10
+#define MAX_DEPTH 5
 
 typedef pair<int,int> pii;
 
@@ -59,6 +59,64 @@ pii minimax(state position,int depth,int alpha,int beta,int player){
     }
 }
 
+//heuristic plays against a player who chooses a move at random
+int heuristic_randomPlayer(int (*heuristic)(state&),int randomPlayer){
+    srand (time(NULL));//seeds time for different rand() value
+
+    state game_state;
+
+    if(randomPlayer == PLAYER_A)
+        game_state = state(heuristic_1,heuristic);//B plays with heuristic
+    else
+        game_state = state(heuristic,heuristic_1);//A plays with heuristic
+
+    while(game_state.is_game_over() < 0){
+        //game_state.print();
+
+        if(game_state.turn == randomPlayer){
+            // cout << "*****Random Player's MOVE*****" << endl;
+
+            // gets the vailble moves
+            vector<int> available_moves;
+            for(int i = 0;i < 6;i++)
+                if(game_state.holes[randomPlayer + i])
+                    available_moves.push_back(randomPlayer + i);
+
+            int random_number = abs(rand()) % ((int) available_moves.size());
+            int rand_move = available_moves[random_number];
+
+            // cout << "Random Player Moved: " << rand_move+1 << endl;
+            game_state = game_state.move_from(rand_move);
+            // game_state.print();
+        }else{
+            // cout << "*****Heuristic MOVE*****" << endl;
+
+            int opt_move = minimax(game_state,
+                                   MAX_DEPTH ,-INF,INF,
+                                   game_state.turn).first;
+
+            // cout << "Heuristic Moved: " << opt_move+1 << endl;
+            game_state = game_state.move_from(opt_move);
+        }
+        // game_state.print();
+    }
+
+    // game_state.print();
+    int winner = game_state.declare_winner(game_state.is_game_over());
+
+    if(winner == -1){
+        // cout << "*****Draw!*****" << endl;
+        return 0;
+    }
+    else if(winner == randomPlayer){
+        // cout << "***Random Player WON!!***" << endl;
+        return -1;
+    }
+    else{
+        // cout << "***Heuristic WON!!***" << endl;
+        return 1;
+    }
+}
 
 //play as palyer(A or B) with heuristic 1 against other with heuristic_2
 void play_with_other(int player,int (*heuristic_a)(state&),int (*heuristic_b)(state&)){
@@ -99,12 +157,13 @@ void play_with_other(int player,int (*heuristic_a)(state&),int (*heuristic_b)(st
         cout << "***you lose!***" << endl;
 
 }
-void heuristic_vs_heuristic(int (*heuristic_a)(state&),int (*heuristic_b)(state&)){
+//0->draw,-1 left heuristic won,+1 right heuristic won
+int heuristic_vs_heuristic(int (*heuristic_a)(state&),int (*heuristic_b)(state&)){
     state game_state(heuristic_a,heuristic_b);
 
     while(game_state.is_game_over() < 0){
         int opt_move = minimax(game_state,MAX_DEPTH,-INF,INF,game_state.turn).first;
-        string turn = (game_state.turn == PLAYER_A)?string("PLAYER_A->") : string("PLAYER_B->");
+        //string turn = (game_state.turn == PLAYER_A)?string("PLAYER_A->") : string("PLAYER_B->");
         //cout << turn << "to move " << opt_move << endl;
         game_state = game_state.move_from(opt_move);
         //game_state.print();
@@ -113,15 +172,55 @@ void heuristic_vs_heuristic(int (*heuristic_a)(state&),int (*heuristic_b)(state&
     int winner = game_state.declare_winner(game_state.is_game_over());
     game_state.print();
     if(winner == -1){
-        cout << "Draw!" << endl;
+        //cout << "Draw!" << endl;
+        return  0;
     }else if(winner == PLAYER_A){
-        cout << "WINNER! PLAYER_A" << endl;
+        //cout << "WINNER! PLAYER_A" << endl;
+        return -1;
     }else if(winner == PLAYER_B){
-        cout << "WINNER! PLAYER_B" << endl;
+        //cout << "WINNER! PLAYER_B" << endl;
+        return +1;
     }
 }
 
+void play_n_games_with_randomPlayer(int n,int heuristic_number,int player){
+    int win,lose,draw;
+    win = lose = draw = 0;
+    for(int i = 0;i < n;i++){
+        int res;
+        switch (heuristic_number) {
+            case 1:
+                res = heuristic_randomPlayer(heuristic_1,player);break;
+            case 2:
+                res = heuristic_randomPlayer(heuristic_2,player);break;
+            case 3:
+                res = heuristic_randomPlayer(heuristic_3,player);break;
+            case 4:
+                res = heuristic_randomPlayer(heuristic_4,player);break;
+            default:
+                cout << "No Heuristic With Number!! " << heuristic_number << endl;
+                return;
+        }
+             if(res ==  0) draw++;
+        else if(res ==  1) win++;
+        else if(res == -1) lose++;
+    }
+    printf("*************Heuristic_%d Stat*************\n",heuristic_number);
+    printf("Game Played %d\n",n);
+    printf("Game Won    %d\n",win);
+    printf("Game Lose   %d\n",lose);
+    printf("Game Drawn  %d\n",draw);
+    printf("Win Ration  %.2lf\n\n",(win*100.0)/n);
+}
+
 int main(int argc, char const *argv[]) {
-    play_with_other(PLAYER_A,heuristic_3,heuristic_3);
+
+    //play_with_other(PLAYER_A,heuristic_3,heuristic_3);
+
+    play_n_games_with_randomPlayer(100,1,PLAYER_B);
+    play_n_games_with_randomPlayer(100,2,PLAYER_B);
+    play_n_games_with_randomPlayer(100,3,PLAYER_B);
+    play_n_games_with_randomPlayer(100,4,PLAYER_B);
+
     return 0;
 }
